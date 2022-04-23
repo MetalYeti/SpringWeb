3dc199d0ad49d0896510eb2727490b699dbffe13
Andrey Bezrukov <andruxa.ic@gmail.com>
2022-04-23 11:35:33 +0400

    ДЗ 9

--- lesson07/src/main/java/ru/geekbrains/persist/ProductRepository.java
+++ lesson07/src/main/java/ru/geekbrains/persist/ProductRepository.java
@@ -1,12 +1,8 @@
 package ru.geekbrains.persist;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
